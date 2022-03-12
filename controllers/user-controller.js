be9aa3a578819d8b82bded5dd368d7f512418186
vayne <miguel.osorio19@yahoo.com>
2022-03-12 16:51:04 -0600

    updated the get all users route, setting up populate method on user for thoughts and friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,6 +7,15 @@ const userController = {
     // Get all Users
     getAllUsers(req, res) {
         User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
