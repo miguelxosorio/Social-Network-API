1f21f7b856f53d52d6c5883b470523a4ef214b95
vayne <miguel.osorio19@yahoo.com>
2022-03-12 00:35:13 -0600

    set up mongoose mongo queries to log

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json()); // convert request body to JSON
 app.use(express.urlencoded({ extended: true })); // also converts request body to JSON  - extended true = post nested objects
 
+// mongoose connects when app is started - tells Mongoose which database we want to connect to.
+// MongoDB will find and connect to the database if it exists or create the database if it doesn't.
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+// use this to log mongo queries being executed!
+// enable logging collection methods + arguments to the console/file
+mongoose.set('debug', true);
 
 // listens for connections on the specified host and port
 app.listen(PORT, ()=> console.log(`🎯 Connected on localhost:${PORT}`));
