d7e82f9439f062f2a4ca13cc2b1f77a7f696876c
vayne <miguel.osorio19@yahoo.com>
2022-03-12 15:14:18 -0600

    setting up the userController object to set up the callback functions for the user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,15 +4,25 @@ const { User, Thought } = require('../models');
 // Controller
 const userController = {
     // Get all Users
-
+    getAllUsers(req, res) {
+        User.find({})
+    },
     // Get a single user by its _id and populated thoought and friend data
-
+    getUserById(req, res) {
+        User.findOne()
+    },
     // Post a new user
-
+    createUser() {
+        User.create()
+    },
     // Put user by its _id
-
+    updateUser() {
+        User.findOneAndUpdate()
+    },
     // Delete user by its _id
-
+    deleteUser() {
+        User.findOneAndDelete()
+    }
     // Bonus remove a user's associated thoughts when deleted - remove thoughts when user is deleted
 }
 
