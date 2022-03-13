cdfd9a01080633f3e463e81fbd33e2ea9059d227
vayne <miguel.osorio19@yahoo.com>
2022-03-13 03:53:16 -0500

    setting up the thought controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,17 @@
+// Importing User and Thought model
+const { User, Thought } = require('../models');
+
+// controller
+const thoughtController = {
+
+    // GET all thoughts
+
+    // Get to get a single thought by its id
+
+    // POST to create a new thought(don't forget to push the created thought's _id to the assoc user's thoughts array field)
+
+    // PUT to update a thought by its _id
+
+    // delete to remove a thought by it's _id
+
+};
