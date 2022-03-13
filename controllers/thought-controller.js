9c6834fd0868ec112b356919d90124846238540a
vayne <miguel.osorio19@yahoo.com>
2022-03-13 03:58:19 -0500

    initializing object functions for the thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -5,13 +5,30 @@ const { User, Thought } = require('../models');
 const thoughtController = {
 
     // GET all thoughts
+    getAllThoughts() {
+
+    }, 
 
     // Get to get a single thought by its id
+    getThoughtById() {
 
+    },
+    
     // POST to create a new thought(don't forget to push the created thought's _id to the assoc user's thoughts array field)
+    createThought() {
 
+    },
+    
     // PUT to update a thought by its _id
+    updateThought() {
+
+    },
 
     // delete to remove a thought by it's _id
-
+    deleteThought() {
+        
+    }
 };
+
+// export
+module.exports = thoughtController;
